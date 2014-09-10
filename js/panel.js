660d8bdb34e159011d10bda350bd094c4695d535
aeriqusyairi <aeriqusyairishamsuri@gmail.com>
2014-09-11 01:24:50 +0800

    remove some old file

--- js/panel.js
+++
@@ -1,9 +0,0 @@
-app.controller('PanelController', function(){
-	this.tab = 1;
-	this.selectTab = function(setTab){
-		this.tab = setTab;
-	};
-	this.isSelected = function(checkTab){
-		return this.tab === checkTab;
-	};
-});
