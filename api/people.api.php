ab6c4325cfc5c57daf719f6d45b2ed7ef7c54dca
aeriqusyairi <aeriqusyairishamsuri@gmail.com>
2014-09-11 20:55:30 +0800

    Add friends in peoples list

--- api/people.api.php
+++ api/people.api.php
@@ -3,7 +3,7 @@
  * People's Api
  * @author aeriqusyairi
  */
-header('Content-type: application/json');
+//header('Content-type: application/json');
 
 require_once 'db/DB.php';
 
@@ -17,7 +17,24 @@ if(isset($_GET['scope']) && !empty($_GET['scope'])){
 
     DB::get('people', array(1,'=',1));
     if(!DB::error()){
+      
+      //get the people list
       $peopleList = DB::results();
+
+      //fetch friends for each people
+      foreach ($peopleList as $people) {
+        DB::get('friend', array('people', '=', $people->id));
+        
+        $friends = DB::results();
+        $people->friends = array();
+        foreach ($friends as $friend) {
+          //get the name of each friend
+          DB::get('people', array('id', '=', $friend->friend));
+          $name = DB::first()->name;
+          array_push($people->friends,array("id"=>$friend->friend, "name"=> $name));
+        }
+      }
+
       echo json_encode($peopleList);
     }else{
       error();
