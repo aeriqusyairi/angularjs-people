cb198176389d9ef6de00141f354e21af6ad17cf4
aeriqusyairi <aeriqusyairishamsuri@gmail.com>
2014-09-11 22:56:40 +0800

    Add about page

--- js/route.js
+++ js/route.js
@@ -2,12 +2,23 @@ angular.module('routeApp', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider){
 	$routeProvider
-		.when('/', {
+		.when('/home', {
 			templateUrl: 'template/home.html',
 			controller: 'HomeController',
+		})
+		.when('/about', {
+			templateUrl: 'template/about.html',
+			controller: 'AboutController',
 		});
+
+		// configure html5 to get links working on jsfiddle
+    	$locationProvider.html5Mode(true);
 })
 
 .controller('HomeController', function($scope, $route, $routeParams, $location){
+
+})
+
+.controller('AboutController', function($scope, $route, $routeParams, $location){
 
 });
